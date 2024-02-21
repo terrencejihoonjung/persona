dc854d60ff90c3709732362bb751b863d1a2a42a
terrencejihoonjung <terrencejung928@gmail.com>
2024-02-21 03:37:40 -0800

    restructured dashboard again. removed task list

--- persona-client/src/pages/Dashboard/Dashboard.tsx
+++ persona-client/src/pages/Dashboard/Dashboard.tsx
@@ -5,16 +5,11 @@ function Dashboard() {
   return (
     <div className="p-6 grid grid-rows-12 grid-cols-12 gap-4 h-[calc(100vh-3rem)] overflow-hidden">
       {/* AI Chat Widget - takes up the full height and 5 columns */}
-      <div className="row-span-12 col-span-5 border shadow rounded">
+      <div className="row-span-12 col-span-8 border shadow rounded">
         {/* AI Chat Widget content goes here */}
       </div>
 
       {/* Task Board - takes up the top 8 rows of the remaining columns */}
-      <div className="row-span-12 col-span-3 border shadow rounded">
-        {/* Task Board content goes here */}
-      </div>
-
-      {/* Spotify Widget - takes up the top 2 rows of the last 4 columns */}
       <div className="row-span-5 col-span-4 border shadow rounded">
         {/* Spotify Widget content goes here */}
       </div>
