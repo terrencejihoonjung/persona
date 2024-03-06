d990a72031a4a69586b8371fc2d540dbce44be2c
terrencejihoonjung <terrencejung928@gmail.com>
2024-03-05 18:45:02 -0800

    styling subtask list

--- persona-client/src/pages/Dashboard/TaskWidget/SortableItem.tsx
+++ persona-client/src/pages/Dashboard/TaskWidget/SortableItem.tsx
@@ -19,7 +19,7 @@ function SortableItem({
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: task.id });
 
-  const style = {
+  const transformStyle = {
     // Need to convert transform object into CSS transform string
     transform: CSS.Transform.toString(transform),
     transition,
@@ -28,14 +28,34 @@ function SortableItem({
   return (
     <div
       ref={setNodeRef}
-      style={style}
+      style={transformStyle}
       {...attributes}
       {...listeners}
-      className={`flex items-center justify-start p-3 mt-2 border-gray-400 border rounded-xl w-full ${
-        isDragging ? "cursor-grabbing" : "cursor-grab"
-      }`}
+      className={`flex items-center justify-start mt-2 ${
+        task.type === "task"
+          ? "border-gray-400 border rounded-xl p-3 mt-2"
+          : "p-2 mt-1"
+      } w-full ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
       onClick={() => openEditModal && openEditModal(task as Task)}
     >
+      {task.type === "subtask" && (
+        <button className="mr-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 opacity-50"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+        </button>
+      )}
       <input
         type="checkbox"
         id={task.id}
@@ -44,7 +64,15 @@ function SortableItem({
         onClick={(e) => e.stopPropagation()}
         onChange={(e) => handleCheckboxChange(task.id, e.target.checked)}
       />
-      <p className="flex text-md font-semibold w-full">{task.text}</p>
+      {task.type === "task" ? (
+        <p className="flex text-md font-semibold w-full">{task.text}</p>
+      ) : (
+        <input
+          value={task.text}
+          type="text"
+          className="appearance-none outline-none text-md font-regular w-fit"
+        />
+      )}
     </div>
   );
 }
--- persona-client/src/pages/Dashboard/TaskWidget/TaskModal.tsx
+++ persona-client/src/pages/Dashboard/TaskWidget/TaskModal.tsx
@@ -47,81 +47,102 @@ function TaskModal({ task, onSave, onExit, onDelete }: TaskModalProps) {
     });
   }
 
+  function handleNewTask() {
+    const newSubTask = {
+      id: `subtask-${Date.now()}`,
+      text: "Untitled",
+      completed: false,
+      type: "subtask",
+    };
+  }
+
+  function handleSubtasksTextChange() {}
+
+  function handleSubtaskDelete() {}
+
   return (
     <div className="z-30 fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-      <div className="relative top-20 mx-auto py-4 border w-2/5 shadow-lg rounded-2xl bg-white">
-        <span className="flex justify-end items-center w-full px-4 mb-4">
-          <button onClick={onExit} className="">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
+      <div className="flex flex-col justify-between relative top-20 mx-auto py-4 border w-2/5 max-h-1/2 shadow-lg rounded-2xl bg-white">
+        <div className="flex flex-col">
+          <span className="flex justify-end items-center w-full px-4 mb-4">
+            <button onClick={onExit} className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </span>
+          <div className="flex flex-col px-8">
+            <input
+              type="text"
+              name="text"
+              value={taskState.text}
+              onChange={handleInputChange}
+              className="appearance-none outline-none font-bold text-3xl mb-10"
+              placeholder="Task Name"
+            />
+
+            <label
+              htmlFor="description"
+              className="block text-xl font-semibold mb-2 pb-1 border-b"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </span>
-        <div className="flex flex-col px-8">
-          <input
-            type="text"
-            name="text"
-            value={taskState.text}
-            onChange={handleInputChange}
-            className="appearance-none outline-none font-bold text-3xl mb-10"
-            placeholder="Task Name"
-          />
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={taskState.description}
+              onChange={handleInputChange}
+              className="w-full px-3 py-2 mb-8 border-gray-400 border rounded-lg"
+              placeholder="Description"
+            ></textarea>
 
-          <label
-            htmlFor="description"
-            className="block text-xl font-semibold mb-2"
-          >
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            value={taskState.description}
-            onChange={handleInputChange}
-            className="w-full px-3 py-2 mb-8 border-gray-400 border rounded-2xl"
-            placeholder="Description"
-          ></textarea>
-
-          <h2 className="font-semibold text-xl">Sub-Tasks</h2>
-          <DraggableList
-            items={taskState.subtasks!}
-            onDragEnd={handleSubtasksReorder}
-          >
-            {(isDragging) => (
-              <div className="py-2 w-full h-full overflow-x-hidden overflow-y-auto">
-                {taskState.subtasks!.map((subtask) => (
-                  <SortableItem
-                    key={subtask.id}
-                    task={subtask}
-                    isDragging={isDragging}
-                    handleCheckboxChange={handleCheckboxChange}
-                  />
-                ))}
-              </div>
-            )}
-          </DraggableList>
+            <h2 className="font-semibold text-xl pb-1 border-b">Sub Tasks</h2>
+            <div className="w-full px-4 text-center text-sm font-semibold text-black border-gray-200 border border-t-0 rounded-b-md cursor-pointer hover:bg-black hover:text-white hover:p-3 hover:rounded-b-xl transition-all duration-200 ease-in-out">
+              Add Sub Task
+            </div>
+            <div className="w-full overflow-x-hidden overflow-y-auto">
+              <DraggableList
+                items={taskState.subtasks!}
+                onDragEnd={handleSubtasksReorder}
+              >
+                {(isDragging) => (
+                  <div className="py-2 w-full h-full overflow-x-hidden overflow-y-auto">
+                    {taskState.subtasks!.map((subtask) => (
+                      <SortableItem
+                        key={subtask.id}
+                        task={subtask}
+                        isDragging={isDragging}
+                        handleCheckboxChange={handleCheckboxChange}
+                      />
+                    ))}
+                  </div>
+                )}
+              </DraggableList>
+            </div>
+          </div>
         </div>
+
         <span className="flex justify-end items-center w-full px-4 mt-12 space-x-4">
           <button
             onClick={handleDelete}
-            className="px-4 py-2 text-white bg-red-500 rounded-xl hover:bg-red-700 transition duration-150 ease-in-out"
+            className="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-700 transition duration-150 ease-in-out"
           >
             Delete
           </button>
           <button
             onClick={handleSave}
-            className="px-4 py-2 mr-2 text-white bg-blue-500 rounded-xl hover:bg-blue-700 transition duration-150 ease-in-out"
+            className="px-4 py-2 mr-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700 transition duration-150 ease-in-out"
           >
             Save
           </button>
